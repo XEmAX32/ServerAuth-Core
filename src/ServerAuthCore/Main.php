a2cffe1446bdb0e1ce947eba979b7e721a9ef768
XEmAX32 <XEmAX32@gmail.com>
2015-09-21 20:28:48 +0200

    Create Main.php

---
+++ src/ServerAuthCore/Main.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ServerAuthCore;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
